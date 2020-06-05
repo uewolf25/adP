aaaae317af053bca788b8d5fe462e4c5fe8a75f7
wolf <nyan643.ym25mm10@gmail.com>
2020-06-06 02:27:02 +0900

    add: settings file

--- 0604/pre/Main.java
+++
@@ -1,8 +0,0 @@
-package pre;
-
-public class Main{
-    public static void main(String[] args) {
-        FibonacciList aFibonacciList = new FibonacciList();
-        aFibonacciList.run(args);
-    }
-}
